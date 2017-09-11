1f37a811edd73884bf64c27884736379ee684899
chucklapress <charleslapress@gmail.com>
2017-09-10 22:24:44 -0400

    modified version of audio player

---
+++ js/audioAssets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+    {
+        'title' : 'Episode 18',
+        'url' : 'https://s3.amazonaws.com/dribbble-pods/dribbble1a.mp3'
+    },
+    {
+        'title' : 'Episode 20',
+        'url' : 'https://s3.amazonaws.com/dribbble-pods/dribbble2a.mp3'
+    }
+];
+
+module.exports = audioData;
